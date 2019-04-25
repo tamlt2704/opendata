802b6f6ab0f24656549b7eee8fe0b77de388d8ed
tamlt2704 <tamlt2704@gmail.com>
2019-04-25 23:22:46 +0800

    add bootstrap nav menu

--- website/wapp/dashboard/urls.py
+++ website/wapp/dashboard/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('about', views.about, name='about'),
+        path('contact', views.contact, name='contact'),
+        path('vcb', views.vcbank, name='vcbank'),
 ]
--- website/wapp/dashboard/views.py
+++ website/wapp/dashboard/views.py
@@ -4,4 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    return render(request, 'dashboard/index.html', context)
+    return render(request, 'index.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'contact.html', context)
+
+def vcbank(request):
+    context = {}
+    return render(request, 'vcb.html', context)
